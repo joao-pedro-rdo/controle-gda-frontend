f4a5be4121f643015e86abd84d6c53f01994a78e
AntonniSMoraes <antonnimoraes@gmail.com>
2025-12-16 17:40:06 -0300

    Styled removido de CarCreate.

--- src/routes/CarCreate.js
+++ src/routes/CarCreate.js
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import Navbar from "../components/Navbar";
 import Unauthorized from "../components/Unauthorized";
 import VehicleForm from "../components/VehicleForm";
@@ -18,17 +17,5 @@ const CarCreate = () => {
     </>
   );
 };
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  margin: auto;
-  width: 90%;
-  background-color: rgb(255, 255, 255, 0.8);
-  height: 90vh;
-  margin-top: 10px;
-  border-radius: 20px;
-`;
 
 export default CarCreate;
